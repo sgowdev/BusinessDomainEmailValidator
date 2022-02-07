19a2f0a33af3b667fb65fc9591bb6890ec41d3b1
Stephen Gowen <dev.sgowen@gmail.com>
2022-02-07 16:40:59 -0600

    Update ActivityExtensions.kt

--- app/src/main/java/com/zello/android/businessdomainemailvalidator3000/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/zello/android/businessdomainemailvalidator3000/extensions/ActivityExtensions.kt
@@ -2,35 +2,7 @@ package com.zello.android.businessdomainemailvalidator3000.extensions
 
 import android.app.Activity
 import android.view.View
-import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
-import android.widget.ProgressBar
-import com.zello.android.businessdomainemailvalidator3000.R
-
-fun Activity.disableUserInteraction() {
-    window.setFlags(
-        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-    )
-}
-
-fun Activity.enableUserInteraction() {
-    window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-}
-
-fun Activity.showIOIndicator() {
-    runOnUiThread {
-        disableUserInteraction()
-        findViewById<ProgressBar>(R.id.loading)?.visibility = View.VISIBLE
-    }
-}
-
-fun Activity.hideIOIndicator() {
-    runOnUiThread {
-        enableUserInteraction()
-        findViewById<ProgressBar>(R.id.loading)?.visibility = View.GONE
-    }
-}
 
 fun Activity.hideKeyboard(view: View) {
     val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
