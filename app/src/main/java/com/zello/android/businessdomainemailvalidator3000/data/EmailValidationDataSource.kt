f38f4b4cb64e847bf69070f284546cac9fb90871
Stephen Gowen <dev.sgowen@gmail.com>
2022-02-07 15:30:52 -0600

    Fixing bug

--- app/src/main/java/com/zello/android/businessdomainemailvalidator3000/data/EmailValidationDataSource.kt
+++ app/src/main/java/com/zello/android/businessdomainemailvalidator3000/data/EmailValidationDataSource.kt
@@ -3,13 +3,16 @@ package com.zello.android.businessdomainemailvalidator3000.data
 import com.github.kittinunf.fuel.Fuel
 import com.google.gson.Gson
 import com.zello.android.businessdomainemailvalidator3000.data.model.EmailValidationResponse
+import org.json.JSONObject
 
 class EmailValidationDataSource {
     suspend fun submitEmailForValidation(email: String): Result<EmailValidationResponse> {
+        val json = JSONObject()
+        json.put("email", email)
         val (request, response, result) = Fuel.post(
-            "https://zello.com/check_email.php",
-            listOf("email" to email)
-        ).response()
+            "https://zello.com/check_email.php"
+        ).body(json.toString())
+            .response()
         val (bytes, error) = result
         return if (error == null) {
             val json = String(bytes!!)
